34654a261e3472c9ee1d4189613f57e16bde715f
Chris Funk <wcfunk@colostate.edu@c3cpu-a5-u34-3.rc.int.colorado.edu>
2024-02-10 13:20:01 -0700

    completed fastp script and sha1_fastqs.txt files

--- assignments/004-iteration-and-fastp/script.sh
+++ assignments/004-iteration-and-fastp/script.sh
@@ -1,8 +1,10 @@
-
+#!/bin/bash
 
 # this assumes that you created an environment
 # called fastp in class on Tuesday.  If you accidentally
 # installed fastp into base, then you don't need this line.
+
+source ~/.bashrc
 conda activate fastp
 
 # This just makes the necessary output directories
@@ -15,7 +17,7 @@ mkdir -p results/trimmed results/qc/fastp
 # DPCh_plate1_B10_S22 DPCh_plate1_B11_S23 DPCh_plate1_B12_S24 DPCh_plate1_C10_S34 ...
 # WITH ALL THE SAMPLE NAME THERE
 
-SAMPLES=..your code goes here..
+SAMPLES=$(basename -s _R1.fq.gz -a data/fastqs/*_R1.fq.gz)
 
 
 # NOW, MODIFY THE FOLLOWING, USING VARIABLE SUBSTITUTION SO THAT S IS USED IN PLACE OF THE SAMPLE
@@ -23,14 +25,13 @@ SAMPLES=..your code goes here..
 # need to wrap S in curly braces!)
 
 for S in $SAMPLES; do
-    fastp -i data/fastqs/DPCh_plate1_B10_S22_R1.fq.gz -I data/fastqs/DPCh_plate1_B10_S22_R2.fq.gz  \
-          -o results/trimmed/DPCh_plate1_B10_S22_R1.fq.gz -O results/trimmed/DPCh_plate1_B10_S22_R2.fq.gz \
-          -h results/qc/fastp/DPCh_plate1_B10_S22.html  -j results/qc/fastp/DPCh_plate1_B10_S22.json \
+    fastp -i data/fastqs/${S}_R1.fq.gz -I data/fastqs/${S}_R2.fq.gz  \
+          -o results/trimmed/${S}_R1.fq.gz -O results/trimmed/${S}_R2.fq.gz \
+          -h results/qc/fastp/${S}.html  -j results/qc/fastp/${S}.json \
           --adapter_sequence=AGATCGGAAGAGCACACGTCTGAACTCCAGTCA --adapter_sequence_r2=AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
           --detect_adapter_for_pe \
-           --cut_right --cut_right_window_size 4 --cut_right_mean_quality 20    
+           --cut_right --cut_right_window_size 4 --cut_right_mean_quality 20
 done
-
 
 # Once that is done, we will sha1sum the trimmed files and store the result
 # in the assignments folder
