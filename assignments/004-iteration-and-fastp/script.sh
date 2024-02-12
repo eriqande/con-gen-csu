b73781fd5383746f93346a12a09e1c578f6aa598
hmoyle <hmoyle@ucsc.edu>
2024-02-12 09:26:11 -0800

    assignment 004, issue with activating fastq conda environment but otherwise works

--- assignments/004-iteration-and-fastp/script.sh
+++ assignments/004-iteration-and-fastp/script.sh
@@ -1,9 +1,8 @@
-
-
+module load fastp
 # this assumes that you created an environment
 # called fastp in class on Tuesday.  If you accidentally
 # installed fastp into base, then you don't need this line.
-conda activate fastp
+#conda init; conda activate fastp
 
 # This just makes the necessary output directories
 mkdir -p results/trimmed results/qc/fastp
@@ -15,7 +14,7 @@ mkdir -p results/trimmed results/qc/fastp
 # DPCh_plate1_B10_S22 DPCh_plate1_B11_S23 DPCh_plate1_B12_S24 DPCh_plate1_C10_S34 ...
 # WITH ALL THE SAMPLE NAME THERE
 
-SAMPLES=..your code goes here..
+SAMPLES="$(basename -s _R1.fq.gz ~/con-gen-csu-hmoyle/data/fastqs/*_R1.fq.gz)"
 
 
 # NOW, MODIFY THE FOLLOWING, USING VARIABLE SUBSTITUTION SO THAT S IS USED IN PLACE OF THE SAMPLE
@@ -23,9 +22,9 @@ SAMPLES=..your code goes here..
 # need to wrap S in curly braces!)
 
 for S in $SAMPLES; do
-    fastp -i data/fastqs/DPCh_plate1_B10_S22_R1.fq.gz -I data/fastqs/DPCh_plate1_B10_S22_R2.fq.gz  \
-          -o results/trimmed/DPCh_plate1_B10_S22_R1.fq.gz -O results/trimmed/DPCh_plate1_B10_S22_R2.fq.gz \
-          -h results/qc/fastp/DPCh_plate1_B10_S22.html  -j results/qc/fastp/DPCh_plate1_B10_S22.json \
+    fastp -i data/fastqs/${S}_R1.fq.gz -I data/fastqs/${S}_R2.fq.gz  \
+          -o results/trimmed/${S}_R1.fq.gz -O results/trimmed/${S}_R2.fq.gz \
+          -h results/qc/fastp/${S}.html  -j results/qc/fastp/${S}.json \
           --adapter_sequence=AGATCGGAAGAGCACACGTCTGAACTCCAGTCA --adapter_sequence_r2=AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
           --detect_adapter_for_pe \
            --cut_right --cut_right_window_size 4 --cut_right_mean_quality 20    
