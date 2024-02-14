ad101b77343017fc8978672202fc660495015640
Sam_Rosenbaum <samwrosenbaum@gmail.com>
2024-02-14 18:44:32 +0000

    Relative paths fixed

--- assignments/004-iteration-and-fastp/script.sh
+++ assignments/004-iteration-and-fastp/script.sh
@@ -12,7 +12,7 @@
 eval "$(conda  shell.bash hook)"
 conda activate fastp
 
-ls /hellgate/home/sr221061/con-gen-csu/data/fastqs | cut -d '_' -f 1-4 | sort -u > sample_names.txt
+ls data/fastqs | cut -d '_' -f 1-4 | sort -u > sample_names.txt
 
 JOBS_FILE=sample_names.txt
 SAMPLES=$(cat ${JOBS_FILE})
@@ -20,15 +20,18 @@ SAMPLES=$(cat ${JOBS_FILE})
 for S in ${SAMPLES};
 do
 
-fastp -i /hellgate/home/sr221061/con-gen-csu/data/fastqs/${S}_R1.fq.gz \
-      -o /hellgate/home/sr221061/con-gen-csu/results/trimmed/${S}_R1_trimmed.fq.gz \
-      -I /hellgate/home/sr221061/con-gen-csu/data/fastqs/${S}_R2.fq.gz \
-      -O /hellgate/home/sr221061/con-gen-csu/results/trimmed/${S}_R2_trimmed.fq.gz \
-      -h /hellgate/home/sr221061/con-gen-csu/results/qc/fastp/${S}.html \
-      -j /hellgate/home/sr221061/con-gen-csu/results/qc/fastp/${S}.html.json \
+fastp -i data/fastqs/${S}_R1.fq.gz \
+      -o results/trimmed/${S}_R1_trimmed.fq.gz \
+      -I data/fastqs/${S}_R2.fq.gz \
+      -O results/trimmed/${S}_R2_trimmed.fq.gz \
+      -h results/qc/fastp/${S}.html \
+      -j results/qc/fastp/${S}.html.json \
       --adapter_sequence=AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \
       --adapter_sequence_r2=AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
       --cut_right --cut_window_size 4
 done
 
-sha1sum /hellgate/home/sr221061/con-gen-csu/results/trimmed/*.gz > /hellgate/home/sr221061/con-gen-csu/assignments/004-iteration-and-fastp/sha1_fastqs.txt
+sha1sum results/trimmed/*.gz > sha1_fastqs.txt
+
+
+
