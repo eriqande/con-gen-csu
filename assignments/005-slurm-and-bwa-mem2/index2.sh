fd2231fc82026df32a7d4cc7b883ffbeb9d92a35
isabelra1 <isabelra@colostate.edu>
2024-02-23 15:18:23 -0700

    Done with homework assignment 005

---
+++ assignments/005-slurm-and-bwa-mem2/index2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2:00:00
+#SBATCH --mem=3G
+#SBATCH --output=bwa_index-%j.out
+#SBATCH --error=bwa_index-%j.err
+
+source ~/.bashrc
+
+conda activate bwa-mem2
+
+
+# make a directory for log output if it does not already exist
+DIR=results/log/bwa_index2
+mkdir -p $DIR
+
+# run bwa index on the input
+bwa-mem2 index $1 > $DIR/log.txt 2>&1
---
+++ assignments/005-slurm-and-bwa-mem2/trim_all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=trimming
+#SBATCH --output=trimming-%j.out
+#SBATCH --error=trimming-%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=4GB
+
+mkdir -p results/trimmed2
+
+
+# activate fastp 
+source ~/.bashrc
+conda activate fastp
+
+
+SAMPLES=$(basename -a -s _R1.fq.gz data/fastqs/*R1.fq.gz)
+
+
+for S in $SAMPLES; do
+    fastp -i data/fastqs/${S}_R1.fq.gz -I data/fastqs/${S}_R2.fq.gz  \
+          -o results/trimmed2/${S}_R1.fq.gz -O results/trimmed2/${S}_R2.fq.gz \
+          -h results/qc/fastp/${S}.html  -j results/qc/fastp/${S}.json \
+          --adapter_sequence=AGATCGGAAGAGCACACGTCTGAACTCCAGTCA --adapter_sequence_r2=AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
+          --detect_adapter_for_pe \
+           --cut_right --cut_right_window_size 4 --cut_right_mean_quality 20    
+
+done
+
